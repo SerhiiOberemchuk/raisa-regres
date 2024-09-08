b44559e1da4191f4c4c156d40abda4d74b793f3d
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-09-08 21:48:09 +0200

    add first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,11 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
+        <h1 className={styles.author}>Раїса Оберемчук</h1>
+        <ol className={styles.servicesList}>
+          <li>Регресія</li>
+          <li>Прогресія</li>
+          <li>Веселка</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -34,62 +26,14 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Замов своє світле майбутнє
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <button type="button" className={styles.secondary}>
+            Читати умови
+          </button>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
---
+++ src/components/Readme.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Readme({}: Props) {
+  return <div>Readme</div>;
+}
